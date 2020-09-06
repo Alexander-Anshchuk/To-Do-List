92b32f8f4e3b1b1c6154fb194974a9c63c1ecaa9
Alexander <naxelord@gmail.com>
2020-09-06 12:11:21 +0300

    adding line through

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
-var input = document.getElementById("userinput");
 var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var li = document.querySelectorAll("li");
-function inputLength (){
-    return input.value.length;
-}
-function createListElement(){
-    var li = document.createElement("li")
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = ""
-}
-function addListAfterClick(){
-    if (inputLength() > 0){
-        createListElement()
+var items = document.querySelectorAll("li");
+button.addEventListener("click", function(){
+    if (input.value.length > 0){
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value))
+        ul.appendChild(li);
+        input.value = "";
     }
-}
-function addListAfterInput(){
-    if (inputLength() > 0 && event.keyCode === 13 ){
-        createListElement()
+})
+input.addEventListener("keypress", function(){
+    if (input.value.length > 0 && event.keyCode === 13){
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value))
+        ul.appendChild(li);
+        input.value = "";
     }
-}
-function deleteli(){
-     delete li[i];
-}
-button.addEventListener("click", addListAfterClick);
-input.addEventListener("keypress", addListAfterInput);
-for (var i = 0 ; i < li.length; i++) {
-    for (var i = 0 ; i < li.length; i++) {
-        li[i].addEventListener('click' , deleteli)
-     }
- }
+})
+function allitems(){
+    for (var i = 0; i < items.length; i++){
+    items[i].classList.toggle("done")
+    }
+}    
+
+    
